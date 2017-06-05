f4a0d3494aaef7402aa4d5151d4ab5fe40c902fa
John <zheng.a.lei@ericsson.com>
2017-06-05 17:39:05 +0800

    demo2

--- src/test1/Demo2.java
+++ src/test1/Demo2.java
@@ -7,6 +7,9 @@ public class Demo2 {
 	 */
 	public static void main(String[] args) {
 		System.out.println("This is Demo2.");
+		for (int i = 0; i < 12; i++) {
+			System.out.println("print" + i);
+		}
 
 	}
 
