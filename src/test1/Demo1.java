55e9c2cb142b1b0ac29972a8d40dd2bd77b83768
Chenglu Tian <chenglu.tian@ericsson.com>
2017-11-06 14:32:15 +0800

    add map

--- src/test1/Demo1.java
+++ src/test1/Demo1.java
@@ -1,5 +1,8 @@
 package test1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Demo1 {
 
 	/**
@@ -7,7 +10,9 @@ public class Demo1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Map<String,String> map = new HashMap<String,String>();
+		System.out.println("get none key:" + map.get("aa"));
+		
 	}
 
 }
